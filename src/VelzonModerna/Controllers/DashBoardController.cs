5e7c39de78e74044b7a1b6c94ab391d900ad7696
lucasferreiralima <lucasfl2001@gmail.com>
2025-05-06 09:59:49 -0300

    teste

--- src/VelzonModerna/Controllers/DashBoardController.cs
+++ src/VelzonModerna/Controllers/DashBoardController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Velzon.Controllers
+namespace Velzon.Controllers 
 {
     public class DashBoardController : Controller
     { 
